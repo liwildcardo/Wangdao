d47d8fcf10aaef1c8b41e64c0098eae0906c82aa
liwildcardo <liwildcardo@gmail.com>
2023-02-27 01:31:50 +0800

    update SqList

--- datastructures.cpp
+++ datastructures.cpp
@@ -3,3 +3,133 @@
 //
 
 #include "datastructures.h"
+
+void InitSqList(SqList &L)
+{
+    for (ElemType i: L.data)
+        i = 0;
+    L.length = 0;
+}
+
+bool EmptySqList(SqList L)
+{
+    return L.length == 0;
+}
+
+bool FullSqList(SqList L)
+{
+    return L.length == MaxSize;
+}
+
+bool AddElemSqList(SqList &L, ElemType x)
+{
+    if (FullSqList(L)) return false;
+    L.data[L.length++] = x;
+    return true;
+}
+
+bool InsertElemSqList(SqList &L, int pos, ElemType x)
+{
+    if (FullSqList(L) || pos > L.length + 1 || pos < 1) return false;
+    for (int i = L.length; i > pos - 1; i--)
+    {
+        L.data[i] = L.data[i - 1];
+    }
+    L.data[pos - 1] = x;
+    L.length++;
+    return true;
+}
+
+bool DeleteElemSqList(SqList &L, int pos, ElemType &x)
+{
+    if (EmptySqList(L)) return false;
+    x = L.data[pos - 1];
+    for (int i = pos - 1; i < L.length - 1; i++)
+    {
+        L.data[i] = L.data[i + 1];
+    }
+    L.length--;
+    return true;
+}
+
+int LocateElemSqList(SqList L, ElemType e)
+{
+    for (int i = 0; i < L.length; i++)
+    {
+        if (L.data[i] == e) return i + 1;
+    }
+    return 0;
+}
+
+bool InitSeqList(SeqList &L, int maxSize)
+{
+    if (maxSize <= 0) return false;
+    L.data = (ElemType *) malloc(sizeof(ElemType) * maxSize);
+    if (L.data == NULL) return false;
+    L.length = 0;
+    L.maxSize = maxSize;
+    return true;
+}
+
+bool EmptySeqList(SeqList L)
+{
+    return L.length == 0;
+}
+
+bool FullSeqList(SeqList L)
+{
+    return L.length == L.maxSize;
+}
+
+bool AddLengthSeqList(SeqList &L, int length)
+{
+    if (length <= 0) return false;
+    ElemType *oldData = L.data;
+    ElemType *newData = (ElemType *) malloc(sizeof(ElemType) * (L.maxSize + length));
+    if (newData == NULL) return false;
+    for (int i = 0; i < L.length; i++) newData[i] = oldData[i];
+    L.data = newData;
+    L.maxSize += length;
+    free(oldData);
+    return true;
+}
+
+bool AddElemSeqList(SeqList &L, ElemType x)
+{
+    if (FullSeqList(L)) AddLengthSeqList(L, 1);
+    L.data[L.length++] = x;
+    return true;
+}
+
+bool InsertElemSeqList(SeqList &L, int pos, ElemType x)
+{
+    if (pos < 1 || pos > L.length + 1) return false;
+    if (FullSeqList(L)) AddLengthSeqList(L, 1);
+    for (int i = L.length - 1; i >= pos - 1; i--)
+    {
+        L.data[i + 1] = L.data[i];
+    }
+    L.data[pos - 1] = x;
+    L.length++;
+    return true;
+}
+
+bool DeleteElemSeqList(SeqList &L, int pos, ElemType &x)
+{
+    if (EmptySeqList(L) || pos < 1 || pos > L.length) return false;
+    for (int i = pos; i < L.length; i++)
+    {
+        L.data[i - 1] = L.data[i];
+    }
+    L.length--;
+    return true;
+}
+
+int LocateElemSeqList(SeqList &L, ElemType e)
+{
+    for (int i = 0; i < L.length; i++)
+    {
+        if (L.data[i] == e) return i + 1;
+    }
+    return 0;
+}
--- datastructures.h
+++ datastructures.h
@@ -11,15 +11,64 @@
 #define MaxSize 10
 #define ElemType int
 
-/**
- * 数据结构：静态顺序表
- */
+
+// 数据结构：静态顺序表
 typedef struct
 {
     ElemType data[MaxSize];
     int length;
 } SqList;
 
+// 初始化静态顺序表
+void InitSqList(SqList &L);
 
+// 判断静态顺序表是否为空
+bool EmptySqList(SqList L);
+
+// 判断静态顺序表是否为满
+bool FullSqList(SqList L);
+
+// 在静态顺序表的表尾添加新元素
+bool AddElemSqList(SqList &L, ElemType x);
+
+// 在静态顺序表的pos位序插入新元素
+bool InsertElemSqList(SqList &L, int pos, ElemType x);
+
+// 删除静态顺序表的位序为pos的元素，返回被删除的元素x
+bool DeleteElemSqList(SqList &L, int pos, ElemType &x);
+
+// 查找静态顺序表第一个元素值为e的元素，并返回其位序
+int LocateElemSqList(SqList L, ElemType e);
+
+// 数据结构：动态顺序表。表的最大长度可以动态分配
+typedef struct
+{
+    ElemType *data;
+    int length, maxSize;
+} SeqList;
+
+// 初始化一个动态顺序表
+bool InitSeqList(SeqList &L, int maxSize);
+
+// 判断动态顺序表是否为空
+bool EmptySeqList(SeqList L);
+
+// 判断动态顺序表是否为满
+bool FullSeqList(SeqList L);
+
+// 动态顺序表的上限增加length长度
+bool AddLengthSeqList(SeqList &L, int length);
+
+// 在动态顺序表的表尾添加新元素
+bool AddElemSeqList(SeqList &L, ElemType x);
+
+// 在动态顺序表的pos位序插入新元素
+bool InsertElemSeqList(SeqList &L, int pos, ElemType x);
+
+// 删除动态顺序表的位序为pos的元素，返回被删除的元素x
+bool DeleteElemSeqList(SeqList &L, int pos, ElemType &x);
+
+// 查找动态顺序表第一个元素值为e的元素，并返回其位序
+int LocateElemSeqList(SeqList &L, ElemType e);
 
 #endif //WANGDAO_DATASTRUCTURES_H
